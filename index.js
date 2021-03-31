4834ad3c3b8bcbc923ef153af9f8f4d9f79ce3c6
Bahlul Hasanli <Behlulheseni@gmail.com>
2021-03-31 15:30:09 +0400

    reboot

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const match = require('./lib/match');
+module.exports = (val) => (...args) => (not) => {
+  const [, exec] = args.find(([item]) => item(val)) ?? [, not];
 
-const option = match(50)(
-  [(key) => key === 18, () => `You are young`],
-  [(key) => key === 50, () => `You are old`],
-  [(key) => key === 6, () => `You are a child`]
-)(() => `Not found!`);
-
-console.log(option);
+  return exec && typeof exec === 'function' ? exec(val) : [, not];
+};
